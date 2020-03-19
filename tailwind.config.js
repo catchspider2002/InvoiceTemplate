f0fccbbf8fa35c60244d13ed326ff772f4b8704c
Naveen <catchspider2002@gmail.com>
2020-03-20 02:18:01 +0530

    Added editable label and included doc definitions in a separate file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
   theme: {},
-  variants: {},
+  variants: { borderColor: ["responsive", "hover", "focus", "active"], borderWidth: ["responsive", "hover", "focus", "active"] },
   // https://github.com/tailwindcss/custom-forms
   plugins: [require("@tailwindcss/custom-forms")]
 };
